6d967eae8838083f2bbb38c1be919504cace9403
Ricardo Chiquetto do Lago <ricardodolago@gmail.com>
2020-04-22 17:01:51 -0300

    Update quase_nada.ino

--- quase_nada.ino
+++ quase_nada.ino
@@ -1,11 +1,13 @@
+//piscar led em 13
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- pinMode(13, OUTPUT);
+ 
   digitalWrite(13, HIGH);
   delay (500);
   digitalWrite(13, LOW);
