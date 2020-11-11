e8ada2fc11ee2dd46b92ce91f3b1213006f68113
Ricardo Chiquetto do Lago <ricardodolago@gmail.com>
2020-11-11 12:49:24 -0300

    Update digitar_coracao_piscante.ino

--- digitar_coracao_piscante/digitar_coracao_piscante.ino
+++ digitar_coracao_piscante/digitar_coracao_piscante.ino
@@ -1,5 +1,4 @@
-// FUNÇÃO QUE DIGITA UM CORAÇÃO QUE PISCA NO LCD (ÉPOCA DE NAMORO)
-
+// FUNÇÃO QUE DIGITA UM CORAÇÃO QUE PISCA NO LCD (ÉPOCA  QUE EU ESTAVA APAIXONADO)
 
 #include<LiquidCrystal.h>
 LiquidCrystal lcd(12,11,5,4,3,2);
@@ -8,8 +7,6 @@ byte cora[8] = {B00000, B01010, B11111, B11111, B11111, B01110, B00100, B00000 }
 
 byte cori[8] = {B00000, B00000, B00100, B01110, B01110, B00100, B00000, B00000};
 
-
-
 void setup(){
      lcd.begin (16,2);
      lcd.createChar(4,cora);
@@ -17,12 +14,12 @@ void setup(){
      lcd.print("Boa noite!");
             }
 void loop(){
-lcd.write(4);
-delay(400);
-lcd.clear();
-lcd.print("Boa noite!");
-lcd.write(1);
-delay(200);
-lcd.clear();
-lcd.print("Boa noite!");
-           }
+     lcd.write(4);
+     delay(400);
+     lcd.clear();
+     lcd.print("Boa noite!");
+     lcd.write(1);
+     delay(200);
+     lcd.clear();
+     lcd.print("Boa noite!");
+}
